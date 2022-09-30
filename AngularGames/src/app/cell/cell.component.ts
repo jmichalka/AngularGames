a1dd6da36a1ed6fa1142f1b6c660c52f6573168f
Tyler Johnson <TylerTonyJohnson@gmail.com>
2022-09-29 18:57:58 -0700

    Side Projects

--- AngularGames/src/app/cell/cell.component.ts
+++ AngularGames/src/app/cell/cell.component.ts
@@ -1,3 +1,4 @@
+import { preserveWhitespacesDefault } from '@angular/compiler';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
@@ -10,6 +11,7 @@ export class CellComponent implements OnInit {
   @Input() initialValue: Number = 0;
 
   displayValue:Number = 0;
+  isEditing:boolean = false;
 
   constructor() { }
 
@@ -17,5 +19,34 @@ export class CellComponent implements OnInit {
     this.displayValue = this.initialValue;
 
   }
+
+// ---------- EVENTS ----------
+
+  // Mouse enter event
+  handleMouseEnter(event: any) {
+    console.log(event.target);
+    event.target.firstChild.focus();
+  }
+
+  handleMouseLeave(event: any) {
+    this.isEditing = false;
+  }
+
+  handleKeyDown(event: any) {
+    event.preventDefault();
+    console.log(event.target);
+    if (!this.isEditing) {
+      event.target.innerText = event.key.toString();
+      this.isEditing = true;
+    }  else {
+      event.target.innerText += event.key.toString();
+    }
+  }
+
+  check(event: Event) {
+    console.log(event)
+  }
+
+// ---------- FUNCTIONS ----------
 
 }
