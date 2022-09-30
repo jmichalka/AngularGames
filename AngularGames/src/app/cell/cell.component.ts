8325cc95812fa673597ec606c849ded9429d1579
Joseph Michalka <joseph.michalka@gmail.com>
2022-09-29 20:56:44 -0500

    First attempt at ngClass

--- AngularGames/src/app/cell/cell.component.ts
+++ AngularGames/src/app/cell/cell.component.ts
@@ -12,6 +12,11 @@ export class CellComponent implements OnInit {
   
   @ViewChild("value") myDiv: ElementRef;
 
+  leftBorderOn:boolean = false;
+  rightBorderOn:boolean = false;
+  topBorderOn:boolean = false;
+  bottomBorderOn:boolean = true;
+
   ngOnInit(): void {
     this.displayValue = this.initialValue.toString();
   }
@@ -24,6 +29,12 @@ export class CellComponent implements OnInit {
     let number = parseInt(event.key);
     event.preventDefault();
     if (isNaN(number) || number < 0 || number > 9) {
+      // if (event.key == 'l') {
+      //   this.leftBorderOn = !this.leftBorderOn;
+      // }
+      // if (event.key == 'r') {
+      //   this.rightBorderOn = !this.rightBorderOn;
+      // }
       return;
     }
     this.displayValue = event.key.toString();
