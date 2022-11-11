4e674daadc6a283c347225bfdd8714f9c5f956f1
Tyler Johnson <TylerTonyJohnson@gmail.com>
2022-11-10 17:49:47 -0800

    Sliding works

--- AngularGames/src/app/boards/slider-board/slider-board.component.ts
+++ AngularGames/src/app/boards/slider-board/slider-board.component.ts
@@ -1,5 +1,13 @@
+import { Dir, Direction } from '@angular/cdk/bidi';
 import { Component, OnInit, HostListener } from '@angular/core';
 import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+
+const Directions = Object.freeze({
+  Up: Symbol("up"),
+  Down: Symbol("down"),
+  Left: Symbol("left"),
+  Right: Symbol("right")
+})
 
 @Component({
   selector: 'app-slider-board',
@@ -24,15 +32,7 @@ export class SliderBoardComponent implements OnInit {
   constructor(private _snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
-    this.slots = [...Array(this.width)].map((e) => Array(this.height).fill(undefined));
-    this.addCell(1, 0, 2);
-    this.addCell(1, 2, 4);
-    // this.addCell(0, 2, 2);
-    // this.addCell(1, 2, 2);
-    // this.addCell(2, 2, 2);
-    // this.addCell(3, 2, 2);
-    // this.addRandomCell();
-    console.table(this.getFlippedSlots());
+    this.setup();
   }
 
   // ---------- EVENTS ----------
@@ -40,103 +40,122 @@ export class SliderBoardComponent implements OnInit {
   @HostListener('window:keydown.arrowup', ['$event'])
   handleArrowUp(event: KeyboardEvent | MouseEvent) {
     event.preventDefault();
-
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-
-        if (this.slots[i][j] === undefined) {
-          continue;
-        }
-
-        for (let v = j - 1; v >= 0; v--) {
-          if (this.slots[i][v] === undefined) {
-            this.moveCell(i, v + 1, i, v);
-          } else if (this.slots[i][v + 1].value === this.slots[i][v].value) {
-            // this.mergeCells(i, v + 1, i, v);
-          }
-        }
-      }
-    }
-    this.addRandomCell();
-    this.showSnackBar('arrow up');
+    this.slide(Directions.Up);
   }
 
   @HostListener('window:keydown.arrowleft', ['$event'])
   handleArrowLeft(event: KeyboardEvent | MouseEvent) {
     event.preventDefault();
-
-    for (let i = 0; i < this.width; i++) {
-      for (let j = 0; j < this.height; j++) {
-
-        if (this.slots[i][j] === undefined) {
-          continue;
-        }
-
-        for (let v = i - 1; v >= 0; v--) {
-
-          if (this.slots[v][j] === undefined) {
-            this.moveCell(v + 1, j, v, j);
-          } else if (this.slots[v + 1][j].value === this.slots[v][j].value) {
-            // this.mergeCells(v + 1, j, v, j);
-          }
-        }
-      }
-    }
-    this.addRandomCell();
-    this.showSnackBar('arrow left');
+    this.slide(Directions.Left);
   }
 
   @HostListener('window:keydown.arrowright', ['$event'])
   handleArrowRight(event: KeyboardEvent | MouseEvent) {
     event.preventDefault();
-
-    for (let i = this.width - 1; i >= 0; i--) {
-      for (let j = 0; j < this.height; j++) {
-
-        if (this.slots[i][j] === undefined) {
-          continue;
-        }
-
-        for (let v = i + 1; v < this.width; v++) {
- 
-          if (this.slots[v][j] === undefined) {
-            this.moveCell(v - 1, j, v, j);
-          } else if (this.slots[v - 1][j].value === this.slots[v][j].value) {
-            // this.mergeCells(v - 1, j, v, j);
-          }
-        }
-      }
-    }
-    this.addRandomCell();
-    this.showSnackBar('arrow right');
+    this.slide(Directions.Right);
   }
 
   @HostListener('window:keydown.arrowdown', ['$event'])
   handleArrowDown(event: KeyboardEvent | MouseEvent) {
     event.preventDefault();
-
-    for (let i = 0; i < this.width; i++) {
-      for (let j = this.height - 1; j >= 0; j--) {
-
-        if (this.slots[i][j] === undefined) {
-          continue;
-        }
-
-        for (let v = j + 1; v < this.height; v++) {
-
-          if (this.slots[i][v] === undefined) {
-            this.moveCell(i, v - 1, i, v);
-          } else if (this.slots[i][v - 1].value === this.slots[i][v].value) {
-            // this.mergeCells(i, v - 1, i, v);
-          }
-        }
-      }
-    }
-    this.addRandomCell();
-    this.showSnackBar('arrow down');
+    this.slide(Directions.Down);
   }
 
   // ---------- METHODS ----------
+
+  setup(): void {
+    this.slots = [...Array(this.width)].map((e) => Array(this.height).fill(undefined));
+    this.addCell(1, 0, 2);
+    this.addCell(1, 2, 4);
+    console.table(this.getFlippedSlots());
+  }
+
+  slide(direction) {
+
+    switch (direction) {
+      case Directions.Up:
+        for (let i = 0; i < this.width; i++) {
+          for (let j = 0; j < this.height; j++) {
+    
+            if (this.slots[i][j] === undefined) {
+              continue;
+            }
+    
+            for (let v = j - 1; v >= 0; v--) {
+              if (this.slots[i][v] === undefined) {
+                this.moveCell(i, v + 1, i, v);
+              } else if (this.slots[i][v + 1].value === this.slots[i][v].value) {
+                // this.mergeCells(i, v + 1, i, v);
+              }
+            }
+          }
+        }
+        break;
+      case Directions.Down:
+        for (let i = 0; i < this.width; i++) {
+          for (let j = this.height - 1; j >= 0; j--) {
+    
+            if (this.slots[i][j] === undefined) {
+              continue;
+            }
+    
+            for (let v = j + 1; v < this.height; v++) {
+    
+              if (this.slots[i][v] === undefined) {
+                this.moveCell(i, v - 1, i, v);
+              } else if (this.slots[i][v - 1].value === this.slots[i][v].value) {
+                // this.mergeCells(i, v - 1, i, v);
+              }
+            }
+          }
+        }
+        break;
+      case Directions.Left:
+        for (let i = 0; i < this.width; i++) {
+          for (let j = 0; j < this.height; j++) {
+    
+            if (this.slots[i][j] === undefined) {
+              continue;
+            }
+    
+            for (let v = i - 1; v >= 0; v--) {
+    
+              if (this.slots[v][j] === undefined) {
+                this.moveCell(v + 1, j, v, j);
+              } else if (this.slots[v + 1][j].value === this.slots[v][j].value) {
+                // this.mergeCells(v + 1, j, v, j);
+              }
+            }
+          }
+        }
+        break;
+      case Directions.Right:
+        for (let i = this.width - 1; i >= 0; i--) {
+          for (let j = 0; j < this.height; j++) {
+    
+            if (this.slots[i][j] === undefined) {
+              continue;
+            }
+    
+            for (let v = i + 1; v < this.width; v++) {
+     
+              if (this.slots[v][j] === undefined) {
+                this.moveCell(v - 1, j, v, j);
+              } else if (this.slots[v - 1][j].value === this.slots[v][j].value) {
+                // this.mergeCells(v - 1, j, v, j);
+              }
+            }
+          }
+        }
+        break;
+      default:
+        console.log("Error finding direction to slide");
+    }
+
+    this.checkGameOver();
+    this.addRandomCell();
+
+  }
 
   moveCell(xStart, yStart, xEnd, yEnd): void {
     // Adjustment in list
@@ -188,32 +207,48 @@ export class SliderBoardComponent implements OnInit {
     for (let i = 0; i < this.width; i++) {
       for (let j = 0; j < this.height; j++) {
         if (this.slots[i][j] === undefined) {
-          emptyCells.push(i + this.width * j);
+          emptyCells.push({x: i, y: j});
         }
       }
     }
 
+    if (emptyCells.length < 1) {
+      this.showSnackBar("Game Over");
+      return;
+    }
+
+    console.log(emptyCells);
+
     // Select random empty slot
-    let randomCellIndex = Math.floor(Math.random() * emptyCells.length);
+    let randomEmptyCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
 
-    let xIndex = randomCellIndex % this.height;
-    let yIndex = Math.floor(randomCellIndex / this.height);
+    this.addCell(randomEmptyCell.x, randomEmptyCell.y, this.getRandomStarter());
 
-    // console.log(randomCellIndex);
-    // console.log(emptyCells);
-    // console.log(xIndex, yIndex);
+  }
 
-    let newCell = {
-      x: xIndex,
-      y: yIndex,
-      value: this.getRandomStarter(),
-      hasMerged: false,
-    };
+  checkGameOver(): void {
+    if (!this.slots.some(column => column.includes(undefined))) {
+      console.log("Found none");
+    } else {
+      console.log("Found some");
+    }
+  }
 
-    this.slots[xIndex][yIndex] = newCell;
-    this.cells.push(newCell);
+  addNextCell(): void {
+   
+    for (let i = 0; i < this.width; i++) {
+      for (let j = 0; j < this.height; j++) {
+        if (this.slots[i][j] === undefined) {
+          
+          this.addCell(i, j, this.getRandomStarter());
+          return;
+        } else {
+          
+        }
+      }
+    }
 
-    console.table(this.getFlippedSlots());
+    alert("GAME OVER");
   }
 
   addCell(x, y, val): void {
@@ -226,6 +261,8 @@ export class SliderBoardComponent implements OnInit {
 
     this.slots[x][y] = newCell;
     this.cells.push(newCell);
+
+    console.table(this.getFlippedSlots());
   }
 
   removeCell(x, y) {
